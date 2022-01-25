8d5b7b25e3c950758086bed127288232a83f8a29
Miguel Gagliardo <miguel.gagliardo@signal-ai.com>
2022-01-25 18:04:53 +0000

    Adding working uniswap config

--- interface/src/constants/lists.ts
+++ interface/src/constants/lists.ts
@@ -1,33 +1,5 @@
-export const DEFAULT_TOKEN_LIST_URL = 'https://gist.githubusercontent.com/mgagliardo/954679e483076e9615c8bae039560943/raw/7f2bb36da86cb1bfa2d8c7c7df073706f79611aa/tokens.json';
+export const DEFAULT_TOKEN_LIST_URL = 'https://gist.githubusercontent.com/mgagliardo/954679e483076e9615c8bae039560943/raw/71c6f2475c79ea49b5ebf6648ba9d9f9800c153f/tokens.json';
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL
 ]
-
-// // the Uniswap Default token list lives here
-// export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
-
-// export const DEFAULT_LIST_OF_LISTS: string[] = [
-//   DEFAULT_TOKEN_LIST_URL,
-//   't2crtokens.eth', // kleros
-//   'tokens.1inch.eth', // 1inch
-//   'synths.snx.eth',
-//   'tokenlist.dharma.eth',
-//   'defi.cmc.eth',
-//   'erc20.cmc.eth',
-//   'stablecoin.cmc.eth',
-//   'tokenlist.zerion.eth',
-//   'tokenlist.aave.eth',
-//   'https://www.coingecko.com/tokens_list/uniswap/defi_100/v_0_0_0.json',
-//   'https://app.tryroll.com/tokens.json',
-//   'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
-//   'https://defiprime.com/defiprime.tokenlist.json',
-//   'https://umaproject.org/uma.tokenlist.json'
-// ]
-
-
-// export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/PureStake/moonbeam-uniswap/main/uniswap-interface-moonbeam/src/tokens.json'
-
-// export const DEFAULT_LIST_OF_LISTS: string[] = [
-//   DEFAULT_TOKEN_LIST_URL
-// ]
